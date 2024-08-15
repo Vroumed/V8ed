e0604d93369581bfef946d1e2c510dedead1f230
Dew <natshaladviera@gmail.com>
2024-08-15 18:20:29 +0200

    Log rover readings and add the ability to sub to it

---
+++ V8ed/Models/Rover/RoverReading.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace Vroumed.V8ed.Models.Rover;
+
+public class RoverReading
+{
+  [JsonProperty("battery_voltage")]
+  public double BatteryVoltage { get; set; }
+
+  [JsonProperty("photosensitive")]
+  public int Photosensitive { get; set; }
+
+  [JsonProperty("track_left")]
+  public int TrackLeft { get; set; }
+
+  [JsonProperty("track_middle")]
+  public int TrackMiddle { get; set; }
+
+  [JsonProperty("track_right")]
+  public int TrackRight { get; set; }
+
+  [JsonProperty("ultrasonic_di")]
+  public double UltrasonicDistance { get; set; } // On suppose que c'est une distance
+
+  [JsonProperty("speed")]
+  public int Speed { get; set; }
+
+  [JsonProperty("direction")]
+  public int Direction { get; set; }
+
+  [JsonProperty("headX")]
+  public int HeadX { get; set; }
+
+  [JsonProperty("thrust")]
+  public int Thrust { get; set; }
+}
