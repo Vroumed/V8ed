26d09144e8401f31adafacdecc1e59cb9e36ef6b
Lucas VANESSCHE <96068906+Meliocus@users.noreply.github.com>
2024-09-02 11:11:15 +0200

    add logger class (#7)
    * add logger class
    
    * add logger class
    
    * Apply dotnet format fixes
    
    * change static class to depencie
    
    * Fixed naming
    
    * remove submodule
    
    * Apply dotnet format fixes
    
    * added Logger to DI
    
    ---------
    
    Co-authored-by: Lucas vanessche <ulacs57otaku@gmail.com>
    Co-authored-by: GitHub Actions <actions@github.com>

---
+++ V8ed/Utils/Logger/LogFile.cs
@@ -0,0 +1,8 @@
+﻿namespace Vroumed.V8ed.Utils.Logger;
+
+public enum LogFile
+{
+  Debug,
+  Log,
+  Error
+}
---
+++ V8ed/Utils/Logger/logger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Vroumed.V8ed.Utils.Logger;
+
+public class Logger
+{
+  private readonly Dictionary<LogFile, string> _logFiles = new();
+
+  public Logger()
+  {
+    Directory.CreateDirectory("Logs");
+    foreach (LogFile logFile in Enum.GetValues(typeof(LogFile)).Cast<LogFile>())
+    {
+      string defaultPath = Path.Combine("Logs", $"{logFile}.txt");
+      File.CreateText(defaultPath).Close();
+      _logFiles[logFile] = defaultPath;
+    }
+  }
+
+  public void Log(LogFile logName, string message)
+  {
+    if (!_logFiles.TryGetValue(logName, out string? logFilePath))
+    {
+      Console.WriteLine($"Log file for '{logName}' not registered.");
+      return;
+    }
+
+    try
+    {
+      using (StreamWriter writer = new(logFilePath, true))
+      {
+        string logEntry = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: {message}";
+        writer.WriteLine(logEntry);
+        Console.WriteLine(logEntry);
+      }
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"Failed to write to log file '{logFilePath}': {ex.Message}");
+    }
+  }
+}
